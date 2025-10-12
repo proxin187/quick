6c49238b78253cfb11a476e7c244928e64906681
proxin187 <thugshaker627@gmail.com>
2025-10-12 22:22:00 +0200

    todo: html namespaces

--- packages/html/src/parser/mod.rs
+++ packages/html/src/parser/mod.rs
@@ -7,21 +7,37 @@ use state::InsertionMode;
 use interface::TreeSink;
 
 
-pub struct TreeBuilder<Sink: TreeSink> {
+pub struct TreeBuilder<Handle, Sink: TreeSink> {
     sink: Sink,
     insertion_mode: InsertionMode,
+    open_elements: Vec<Handle>,
 }
 
-impl<Sink: TreeSink> TreeBuilder<Sink> {
-    pub fn new(sink: Sink) -> TreeBuilder<Sink> {
+impl<Handle, Sink: TreeSink> TreeBuilder<Handle, Sink> {
+    pub fn new(sink: Sink) -> TreeBuilder<Handle, Sink> {
         TreeBuilder {
             sink,
             insertion_mode: InsertionMode::Initial,
+            open_elements: Vec::new(),
         }
+    }
+
+    fn current_node(&self) -> &Handle {
+        self.open_elements.last().expect("no current node")
+    }
+
+    // TODO: currently this assumes its not a html fragment parser.
+    fn adjusted_current_node(&self) -> &Handle {
+        self.current_node()
+    }
+
+    // TODO: we need to figure out html namespaces in order to make this function work properly
+    fn not_foreign(&self, token: &Token) -> bool {
+        self.open_elements.is_empty()
     }
 }
 
-impl<Sink: TreeSink> TokenSink for TreeBuilder<Sink> {
+impl<Handle, Sink: TreeSink> TokenSink for TreeBuilder<Handle, Sink> {
     fn process(&mut self, token: Token) {
     }
 
